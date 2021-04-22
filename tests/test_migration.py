b81fdcf70cc79db0efc7599079dc793788780c42
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2021-04-22 21:58:14 +0200

    initial commit

---
+++ tests/test_migration.py
@@ -0,0 +1,23 @@
+# TODO: Add tests that show proper migration of the strategy to a newer one
+#       Use another copy of the strategy to simulate the migration
+#       Show that nothing is lost!
+
+import pytest
+
+
+def test_migration(
+    token, vault, strategy, amount, Strategy, strategist, gov, user, RELATIVE_APPROX
+):
+    # Deposit to the vault and harvest
+    token.approve(vault.address, amount, {"from": user})
+    vault.deposit(amount, {"from": user})
+    strategy.harvest()
+    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+
+    # migrate to a new strategy
+    new_strategy = strategist.deploy(Strategy, vault)
+    strategy.migrate(new_strategy.address, {"from": gov})
+    assert (
+        pytest.approx(new_strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX)
+        == amount
+    )
